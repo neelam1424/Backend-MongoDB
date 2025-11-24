1107afc330ced6fe0300ab1868e16414bec3e515
neelam1424 <neelammore1404@gmail.com>
2025-11-24 15:54:53 -0500

    logger added

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,10 +1,36 @@
 import express from 'express'
+import logger from './logger.js'
+import morgan from 'morgan'
 
 const app = express()
 
 const port = 3000
 //accept data from frontend in json format
 app.use(express.json())
+
+
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+
+
+
+const morganFormat = ":method :url :status :response-time ms";
+
 
 let teaData = []
 let nextId = 1
